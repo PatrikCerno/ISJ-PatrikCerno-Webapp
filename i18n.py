3d51fe0e98fceb382ee74cf2e9d5c5eef027cae1
PatrikCerno <patrik.cerno@student.ssnd.sk>
2025-05-26 12:55:51 +0200

    uprava

--- i18n.py
+++ i18n.py
@@ -1,11 +1,14 @@
 TRANSLATIONS = {
     "sk": {
         "courses_list": "Zoznam kurzov:",
+        "trainer_list": "Zoznam trénerov",
         "back": "Späť"
     },
     "en": {
         "courses_list": "Course list:",
+        "trainer_list": "Trainer list",
         "back": "Back"
     }
 }
+
 SUPPORTED = {"sk", "en"}
